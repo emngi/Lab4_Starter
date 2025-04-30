caf43db57e6c47e1a94890ee3b2ed5d3215e5531
emngi <enguyen1110@gmail.com>
2025-04-30 01:04:58 -0700

    fixed sumValues

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,15 +6,11 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add) {
-        const result = 0;
-
-        result = num1 + num2;
-
-        return result;
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 0;
     }
-    else {
-        return !add;
+    if(add){
+        return num1 + num2;
     }
 }
 
