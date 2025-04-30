24d56b294a1cdfb5fdfc9be6ac70686e55c4e7ac
emngi <enguyen1110@gmail.com>
2025-04-30 01:08:13 -0700

    Fix discountPrices

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -21,11 +21,16 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    const discounted = []
+    if (!Array.isArray(prices) || typeof discount !== 'number' || prices.length === 0) {
+        return false;
+    }
+
+    const discounted = [];
     const length = prices.length;
     let discountedPrice = 0
+
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
